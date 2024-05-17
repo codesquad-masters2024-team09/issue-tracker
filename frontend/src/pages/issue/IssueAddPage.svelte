67c83d35ceaff348728b101e3e5c88eef9262610
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-05-17 11:50:28 +0900

    [feat/#24] feat: 이슈 추가 화면 개발
    - 이슈 추가 페이지를 위한 UI 추가
    - 폼이 모두 작성되어야 제출 버튼이 활성화되는 로직 추가
    - 취소 버튼 클릭시 작성된 내용이 삭제, 기본 페이지로 되돌아가는 로직 추가

---
+++ frontend/src/pages/issue/IssueAddPage.svelte
@@ -0,0 +1,78 @@
+<script>
+    import {issues} from "../../stores/issue.js";
+    import {router} from "tinro";
+
+    let issueFormValues = {
+        title: '',
+        content: '',
+    }
+
+    const onCreateIssue = async () => {
+        try {
+            await issues.createIssue(issueFormValues.title, issueFormValues.content);
+            onCancelCreateIssue();
+        }
+        catch (err) {
+            alert(err);
+        }
+    }
+
+    const onCancelCreateIssue = () => {
+        issueFormValues.title = '';
+        issueFormValues.content = '';
+        router.goto("/");
+    }
+
+    let isSubmitLocked = true;
+    $: {
+        isSubmitLocked = issueFormValues.title.trim() === ''
+            || issueFormValues.content.trim() === '';
+    }
+
+</script>
+
+
+<div id="header-area">
+    <strong id="title-text">새로운 이슈 작성</strong>
+</div>
+<div id="main-area">
+    <div id="content-area">
+        <div class="left-section">
+            <img src="/assets/profile_icon.svg" alt="Profile Icon" class="profile-icon">
+            <div class="issue-header">
+                <input type="text" placeholder="제목" class="title-input" bind:value={issueFormValues.title}>
+            </div>
+            <div class="content-input">
+                <textarea placeholder="내용을 입력하세요" bind:value={issueFormValues.content}></textarea>
+            </div>
+            <div class="attachment">
+                <span>📎 파일 첨부하기</span>
+            </div>
+        </div>
+
+        <div class="action-buttons">
+            <button class="cancel-button"
+                    on:click={onCancelCreateIssue}>✕ 작성 취소</button>
+            <button class="submit-button"
+                    disabled={isSubmitLocked}
+                    on:click={onCreateIssue}>완료</button>
+        </div>
+    </div>
+
+    <div id="additional-info-area">
+        <div class="option-container">
+            <div class="option-item">
+                <span>담당자</span>
+                <button class="add-button">+</button>
+            </div>
+            <div class="option-item">
+                <span>레이블</span>
+                <button class="add-button">+</button>
+            </div>
+            <div class="option-item">
+                <span>마일스톤</span>
+                <button class="add-button">+</button>
+            </div>
+        </div>
+    </div>
+</div>
