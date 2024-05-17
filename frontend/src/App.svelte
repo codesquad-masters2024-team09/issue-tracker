7a9f16c344dd62384044b091aec85e4b9d1a09a0
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-05-17 11:42:25 +0900

    [feat/#24] feat: 기본 라우팅 로직 설정

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,12 +2,14 @@
   import {Route} from "tinro";
   import IssueAddPage from "./pages/issue/IssueAddPage.svelte";
   import Header from "./components/common/Header.svelte";
+  import IssueDetailPage from "./pages/issue/IssueDetailPage.svelte";
 
 </script>
 
 <Header />
 <div id="container">
-  <Route path="/issues/add">
-    <IssueAddPage />
+  <Route path="/issues/*" firstmatch>
+    <Route path="/add"><IssueAddPage/></Route>
+    <Route path="/:issueId"><IssueDetailPage/></Route>
   </Route>
 </div>
