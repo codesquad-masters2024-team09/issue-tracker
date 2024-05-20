b96c0a973d68e71da2e2380dd019c54ae7b0c9a9
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-05-20 16:27:04 +0900

    [feat/#37] feat: 이슈 목록 화면 UI 및 내부 기능 구현

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,11 +3,18 @@
   import IssueAddPage from "./pages/issue/IssueAddPage.svelte";
   import Header from "./components/common/Header.svelte";
   import IssueDetailPage from "./pages/issue/IssueDetailPage.svelte";
+  import IssueListPage from "./pages/issue/IssueListPage.svelte";
 
 </script>
 
 <Header />
 <div id="container">
+  <Route path="/">
+    <IssueListPage />
+  </Route>
+  <Route path="/issues">
+    <IssueListPage />
+  </Route>
   <Route path="/issues/*" firstmatch>
     <Route path="/add"><IssueAddPage/></Route>
     <Route path="/:issueId"><IssueDetailPage/></Route>
