65c553c915b18529bc693dcf81d45acddd1ca2b8
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-05-20 16:26:23 +0900

    [feat/#37] feat: 이슈 목록 필터 기능을 위한 상태관리 store 구현

---
+++ frontend/src/stores/filter.js
@@ -0,0 +1,41 @@
+import { writable, get } from 'svelte/store';
+
+const createOptionsStore = () => {
+    const { subscribe, update } = writable({
+        assignees: [],
+        labels: [],
+        milestones: [],
+        writers: []
+    });
+
+    const updateOption = (category, option) => {
+        update(store => {
+            const index = store[category].indexOf(option);
+            if (index === -1) {
+                store[category].push(option); // 선택한 옵션을 리스트에 추가
+            } else {
+                store[category].splice(index, 1); // 선택한 옵션을 리스트에서 제거
+            }
+            return store;
+        });
+    };
+
+    const logAllSelectedOptions = () => {
+        subscribe(store => {
+            console.log(store);
+        });
+    };
+
+    logAllSelectedOptions();
+
+
+    return {
+        subscribe,
+        toggleAssigneeOption: (option) => updateOption('assignees', option),
+        toggleLabelOption: (option) => updateOption('labels', option),
+        toggleMilestoneOption: (option) => updateOption('milestones', option),
+        toggleWriterOption: (option) => updateOption('writers', option),
+    };
+};
+
+export const optionsStore = createOptionsStore();
