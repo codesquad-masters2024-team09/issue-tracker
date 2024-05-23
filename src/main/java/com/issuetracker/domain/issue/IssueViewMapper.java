4c03f0e10bf224ed9b404bb2b9e17189fbf83395
곽태영 <amberaplin14@gmail.com>
2024-05-23 16:22:40 +0900

    [feat/#44] feat: 뷰에서 필요한 DTO를 가져오는 IssueViewMapper 클래스에 findAll() 정의, mybatis 동적 쿼리 작성

--- src/main/java/com/issuetracker/domain/issue/IssueViewMapper.java
+++ src/main/java/com/issuetracker/domain/issue/IssueViewMapper.java
@@ -1,9 +1,14 @@
 package com.issuetracker.domain.issue;
 
+import com.issuetracker.domain.issue.response.SimpleIssue;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface IssueViewMapper {
 
     IssueDetails findById(Long issueId);
+
+    List<SimpleIssue> findAll();
 }
