736c71c116723e92660e6ff85c24acaa3f50ff8d
곽태영 <amberaplin14@gmail.com>
2024-05-23 11:44:42 +0900

    [feat/#43] feat: 이슈 상세 조회시 필요한 모든 데이터 함께 조회하는 메서드 구현
    - Comment와 IssueLabel, Label 조인으로 관련된 모든 정보를 조회하는 메서드 mybatis로 구현. IssueDetails 타입으로 매핑되어 조회된다
    - IssueDetailsResponse에서 엔티티 클래스 자체의 리스트를 필드로 갖지 않고, 필요한 정보만 갖는 DTO 클래스를 만들어 해당 클래스 리스트를 갖도록 했다. DTO 내부 of 팩토리 메서드에서 해당하는 형태로 변경되어 응답된다.

--- src/main/java/com/issuetracker/domain/issue/IssueService.java
+++ src/main/java/com/issuetracker/domain/issue/IssueService.java
@@ -2,8 +2,9 @@ package com.issuetracker.domain.issue;
 
 import com.issuetracker.domain.issue.request.IssueSearchCondition;
 import java.util.HashMap;
+
+import com.issuetracker.domain.issue.response.IssueDetailsResponse;
 import lombok.RequiredArgsConstructor;
-import com.issuetracker.domain.issue.response.IssueDetailResponse;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.issuetracker.domain.issue.response.IssueListResponse;
@@ -17,15 +18,16 @@ public class IssueService {
 
     private final IssueRepository issueRepository;
     private final IssueMapper issueMapper;
+    private final IssueViewMapper issueViewMapper;
 
     public Long create(Issue issue) {
         Issue savedIssue = issueRepository.save(issue);
         return savedIssue.getId();
     }
 
-    public IssueDetailResponse getDetail(Long issueId) {
-        Issue issue = issueRepository.findById(issueId).orElseThrow(RuntimeException::new);
-        return IssueDetailResponse.from(issue);
+    public IssueDetailsResponse getDetail(Long issueId) {
+        IssueDetails issueDetails = issueViewMapper.findById(issueId);
+        return IssueDetailsResponse.of(issueDetails);
     }
 
     public void delete(Long issueId) {
@@ -44,6 +46,7 @@ public class IssueService {
     }
 
     public void addLabel(Long issueId, String labelId) {
+        // TODO: 레이블이 존재하는지 확인하는 로직 넣어야 함
         Issue issue = issueRepository.findById(issueId).orElseThrow(RuntimeException::new);
         issue.addLabel(labelId);
 
