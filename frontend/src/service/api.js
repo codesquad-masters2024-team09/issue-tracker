eed7d91341bc9fd8abf80278f157cb62f8ac7033
sharpie1330 <amberaplin14@gmail.com>
2024-05-30 05:23:39 +0900

    [feat/#69] fix: api.js 액세스토큰 파라미터 위치 수정

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -35,23 +35,23 @@ const send = async ({method='', path='', data={}, access_token=''} = {}) => {
 }
 
 const getApi = ({path='', access_token=''} = {}) => {
-    return send({method: 'GET', path, access_token})
+    return send({method: 'GET', path: path, access_token: access_token})
 }
 
-const putApi = ({path='', data={}} = {}, access_token='') => {
-    return send({method: 'PUT', path, data, access_token})
+const putApi = ({path='', data={}, access_token=''} = {}) => {
+    return send({method: 'PUT', path: path, data: data, access_token: access_token})
 }
 
-const patchApi = ({path='', data={}} = {}, access_token='') => {
-    return send({method: 'PATCH', path, data, access_token})
+const patchApi = ({path='', data={}, access_token=''} = {}) => {
+    return send({method: 'PATCH', path: path, data: data, access_token: access_token})
 }
 
-const postApi = ({path='', data={}} = {}, access_token='') => {
-    return send({method: 'POST', path, data, access_token})
+const postApi = ({path='', data={}, access_token=''} = {}) => {
+    return send({method: 'POST', path: path, data: data, access_token: access_token})
 }
 
-const delApi = ({path='', data={}} = {}, access_token='') => {
-    return send({method:'DELETE', path, data, access_token})
+const delApi = ({path='', data={}, access_token=''} = {}) => {
+    return send({method:'DELETE', path: path, data: data, access_token: access_token})
 }
 
 export {
